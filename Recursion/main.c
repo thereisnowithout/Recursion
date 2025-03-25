267c311804ba96a133963eb8af4ef7896364537e
Brett Grentell <thereisnowithout@gmail.com>
2025-03-26 00:32:10 +1100

    Adding static variable to result of recursive function

--- Recursion/main.c
+++ Recursion/main.c
@@ -7,8 +7,20 @@
 
 #include <stdio.h>
 
+int fun(int n)
+{
+    static int x=0;
+    if (n > 0)
+    {
+        x++;
+        return fun(n-1)+x;
+    }
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int r;
+    r = fun(5);
+    printf("%.d", r);
     return 0;
 }
